ba7fd8df9312c053ea166f5f20a0d11ba2e2ad43
CrazyBIRB <tiagojralmeida@outlook.com>
2024-05-19 16:48:55 +0100

    Melhoria da pagina login/register

--- js/login.js
+++ js/login.js
@@ -1,9 +1,16 @@
-document.getElementById('show-register').addEventListener('click', () => {
-    document.getElementById('login-form').classList.add('hidden');
-    document.getElementById('register-form').classList.remove('hidden');
-});
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('show-register').addEventListener('click', () => {
+        document.getElementById('login-form').classList.add('hidden');
+        document.getElementById('register-form').classList.remove('hidden');
+    });
 
-document.getElementById('show-login').addEventListener('click', () => {
-    document.getElementById('register-form').classList.add('hidden');
-    document.getElementById('login-form').classList.remove('hidden');
+    document.getElementById('show-login').addEventListener('click', () => {
+        document.getElementById('register-form').classList.add('hidden');
+        document.getElementById('login-form').classList.remove('hidden');
+    });
+
+    document.getElementById('login-button').addEventListener('click', () => {
+        // Redirect to the desired page
+        window.location.href = '../html/index-registered.html'; // Replace with your target URL
+    });
 });
